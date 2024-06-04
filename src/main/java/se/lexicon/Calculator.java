2f1096bf1fb7658b951ba6b884aca7b5de0a37fd
Casper Nilsson <caspernilsson13@gmail.com>
2024-06-04 13:13:38 +0200

    Added new code

--- src/main/java/se/lexicon/Calculator.java
+++ src/main/java/se/lexicon/Calculator.java
@@ -6,11 +6,11 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        while(true) {
+        while (true) {
             System.out.print("Enter an operation (+, -, *, /) or 'exit' to quit ");
             String operation = input.next();
 
-            if(operation.equals("exit")) {
+            if (operation.equals("exit")) {
                 System.out.println("Exiting the calculator");
                 break;
             }
@@ -22,16 +22,16 @@ public class Calculator {
 
             switch (operation) {
                 case "+":
-                    System.out.println("Result: " + (x + y ));
+                    System.out.println("Result: " + (x + y));
                     break;
                 case "-":
-                    System.out.println("Result: " + (x - y ));
+                    System.out.println("Result: " + (x - y));
                     break;
                 case "*":
-                    System.out.println("Result: " + (x * y ));
+                    System.out.println("Result: " + (x * y));
                     break;
                 case "/":
-                    System.out.println("Result: " + (x / y ));
+                    System.out.println("Result: " + (x / y));
                     break;
                 default:
                     System.out.println("Invalid operation. Please enter a valid operation");
